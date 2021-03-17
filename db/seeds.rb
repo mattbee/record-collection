c1771c3bf56ed8b80dc3e532844d2a5dc14d47bd
Matt Bee <matt@todayishould.com>
2021-03-17 21:54:03 +0000

    Add Database seed file to include some initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+conditions = Condition.create([
+  {state: 'Brand New'},
+  {state: 'Nearly New'},
+  {state: 'Good'},
+  {state: 'OK'},
+  {state: 'Bad'},
+  {state: 'Awful'},
+])
+
+artists = Artist.create([{name: 'Nirvana'}, {name: 'Foo Fighters'}])
+
+records = Record.create([
+  {
+    title: 'Nevermind',
+    release_year: 1991,
+    artist: artists.first,
+    condition: conditions.first,
+  },
+  {
+    title: 'The Colour and the Shape',
+    release_year: 1997,
+    artist: artists.last,
+    condition: conditions.third,
+  }
+])
