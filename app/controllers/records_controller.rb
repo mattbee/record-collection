9bf39ca595ff95e12f7b79eef6053d9e5bb0919e
Matt Bee <matt@todayishould.com>
2021-03-17 22:46:26 +0000

    Paginate results
    Using the JSOM Paginator gem, we can easily add JSON API compliant meta
    information around our Records API, including page number, page size,
    and next/previous links to be used in the application interacting with
    the API.
    
    It uses methods deprecated in Ruby 2.7 but as this is a toy app, I'm not
    too worried by this. In a real app I might use `pagy` on its own or get
    the gem updated to be better with Ruby 2.7

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -2,12 +2,26 @@ class RecordsController < ApplicationController
 
   def index
     records = Record.all
-    render json: serializer.new(records), status: :ok
+    paginated_records = paginator.call(
+      records,
+      params: pagination_params,
+      base_url: request.url
+    )
+    options = { meta: paginated_records.meta.to_h, links: paginated_records.links.to_h }
+    render json: serializer.new(paginated_records.items, options), status: :ok
   end
 
   private
 
   def serializer
     RecordSerializer
+  end
+
+  def paginator
+    JSOM::Pagination::Paginator.new
+  end
+
+  def pagination_params
+    params.permit![:page]
   end
 end
--- spec/requests/records_spec.rb
+++ spec/requests/records_spec.rb
@@ -14,17 +14,30 @@ RSpec.describe RecordsController, type: :request do
       get '/records'
       body = JSON.parse(response.body).deep_symbolize_keys
       expect(body).to eq(
-        'data': [{
+        data: [{
           id: record.id.to_s,
-          type: 'record',
           attributes: {
             title: record.title,
             release_year: record.release_year.to_s,
             artist: record.artist.to_s,
             condition: record.condition.to_s
-          }
-        }]
+          },
+          type: 'record',
+        }],
+        links: {
+          self: "http://www.example.com/records"
+        },
+        meta: {
+          pages: 1,
+          total: 1
+        }
       )
+    end
+
+    it 'creates paginated results' do
+      record1, record2, record3 = FactoryBot.create_list(:record, 3)
+      get '/records', {params: { page: { number: 2, size: 1 } } }
+      expect(response.body.to_s).to include("?page[number]=3\\u0026page[size]=1")
     end
   end
 
